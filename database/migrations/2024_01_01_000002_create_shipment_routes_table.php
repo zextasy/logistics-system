a1a8f4e27c60de81002022e918d75cc4e58cf9cd
Ugochukwu Ezekwem <the_zex@yahoo.com>
2024-12-15 15:13:08 +0100

    db - changed enums to strings

--- database/migrations/2024_01_01_000002_create_shipment_routes_table.php
+++ database/migrations/2024_01_01_000002_create_shipment_routes_table.php
@@ -23,7 +23,6 @@ return new class extends Migration
             $table->timestamps();
 
             // Indexes
-            $table->index(['shipment_id', 'order']);
             $table->index('status');
         });
     }
